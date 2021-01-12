e8d22cc76e27f04a629f71cfe729983a2e122694
Dariusz <dariuszklimaszewski@wp.pl>
2021-01-12 16:31:25 +0100

    Second commit

--- src/main/java/pl/sda/java/project/finalproject/exceptions/UserWithSuchEmailExistsExepction.java
+++ src/main/java/pl/sda/java/project/finalproject/exceptions/UserWithSuchEmailExistsExepction.java
@@ -1,10 +1,7 @@
 package pl.sda.java.project.finalproject.exceptions;
 
-import javax.validation.constraints.Email;
-
 public class UserWithSuchEmailExistsExepction extends RuntimeException{
-//    public UserWithSuchEmailExistsExepction(@Email(message = "User already exists") String email) {
-//    }
+
     private final String email;
 
     public UserWithSuchEmailExistsExepction(String email) {
